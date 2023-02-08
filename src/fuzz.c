12753e2c5d4b68e81761c4b3a6b0a47367686db7
ianjray <ianjray@me.com>
2023-02-08 22:26:53 +0200

    Initial commit
    Signed-off-by: Ian Ray <ianjray@me.com>

---
+++ src/fuzz.c
@@ -0,0 +1,177 @@
+#include "false.h"
+
+#include "code-point.h"
+
+#include <assert.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+static void nop_fatal(const struct config config, const char *pos, const char *msg)
+{
+    (void)config;
+    (void)pos;
+    (void)msg;
+}
+
+static void nop_log_trace(const struct config config, wchar_t wc, const char *pos)
+{
+    (void)config;
+    (void)wc;
+    (void)pos;
+}
+
+static void nop_log_stack(const struct config config, const char *op, const char *dump)
+{
+    (void)config;
+    (void)op;
+    (void)dump;
+}
+
+static void nop_emit_number(int x)
+{
+    (void)x;
+}
+
+static void nop_emit_wchar(wchar_t wc)
+{
+    (void)wc;
+}
+
+static void nop_emit_char(char c)
+{
+    (void)c;
+}
+
+static int nop_input(void)
+{
+    return 1;
+}
+
+static void nop_flush(void)
+{
+}
+
+int main(void)
+{
+    /* Ad-hoc fuzz test of all supported symbols.
+     * The iterations and range variables were chosen to get good code coverage.
+     * This test aims to identify crashes caused by malformed input. */
+
+    static const wchar_t operator_array[] = {
+        '`',
+        '\\',
+        '$',
+        '%',
+        '@',
+        LATIN_SMALL_LETTER_O_WITH_STROKE,
+        'O',
+        '+',
+        '-',
+        '*',
+        '/',
+        '=',
+        '>',
+        '&',
+        '|',
+        '_',
+        '~',
+        '^',
+        '.',
+        ',',
+        LATIN_SMALL_LETTER_SHARP_S,
+        'B',
+        ':',
+        ';',
+        '{',
+        '\'',
+        '"',
+        '[',
+        ']',
+        '}',
+        '!',
+        '?',
+        '#',
+    };
+
+    char *args[] = { "stdin" };
+
+    const size_t iterations = 10000;
+    const int range = 500;
+
+    size_t ws_size = 1;
+    size_t digit_size = 10;
+    size_t letter_size = 26;
+    size_t operator_size = sizeof(operator_array) / sizeof(*operator_array);
+    size_t invalid_size = 1;
+    size_t alphabet_size = ws_size + digit_size + letter_size + operator_size + invalid_size;
+
+    struct config config;
+
+    config.argc = sizeof(args) / sizeof(*args);
+    config.argv = args;
+
+    config.fatal       = nop_fatal;
+    config.log_trace   = nop_log_trace;
+    config.log_stack   = nop_log_stack;
+    config.emit_number = nop_emit_number;
+    config.emit_wchar  = nop_emit_wchar;
+    config.emit_char   = nop_emit_char;
+    config.input       = nop_input;
+    config.flush       = nop_flush;
+
+    // Test data includes UTF-8 encoded multibyte characters.
+    setlocale(LC_ALL, "en_US.UTF-8");
+
+    srand(0);
+
+    for (size_t iteration = 0; iteration < iterations; ++iteration) {
+        size_t len = (size_t)(rand() % range);
+        char *buf = (char *)malloc(len * 4);  // Include overhead for multibyte characters.
+        size_t width = 0;
+
+        for (size_t i = 0; i < len; ++i) {
+            size_t r = (size_t)((size_t)rand() % alphabet_size);
+            wchar_t wc;
+
+            if (r < ws_size) {
+                wc = ' ';
+            } else {
+                r -= ws_size;
+                if (r < digit_size) {
+                    wc = '0' + (char)r;
+                } else {
+                    r -= digit_size;
+                    if (r < letter_size) {
+                        wc = 'a' + (char)r;
+                    } else {
+                        r -= letter_size;
+                        if (r < operator_size) {
+                            wc = operator_array[r];
+                        } else {
+                            r -= operator_size;
+                            if (r < invalid_size) {
+                                wc = 'A';
+                            } else {
+                                r -= invalid_size;
+                                abort();
+                            }
+                        }
+                    }
+                }
+            }
+
+            width += wcrtomb(&buf[width], wc, NULL);
+        }
+
+        buf[width] = 0;
+
+        config.str = buf;
+        interpret(config);
+        free(buf);
+    }
+
+    return 0;
+}
---
+++ src/test_false.c
@@ -0,0 +1,360 @@
+#include "false.h"
+
+#include "storage.h"
+
+#include <assert.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+static void nop_fatal(const struct config config, const char *pos, const char *msg)
+{
+    (void)config;
+    (void)pos;
+    (void)msg;
+}
+
+static void nop_log_trace(const struct config config, wchar_t wc, const char *pos)
+{
+    (void)config;
+    (void)wc;
+    (void)pos;
+}
+
+static void nop_log_stack(const struct config config, const char *op, const char *dump)
+{
+    (void)config;
+    (void)op;
+    (void)dump;
+}
+
+static const char *buffered_input;
+
+static int input(void)
+{
+    if (buffered_input) {
+        if (*buffered_input) {
+            return *buffered_input++;
+        }
+        buffered_input = NULL;
+    }
+
+    return -1;
+}
+
+static void nop_flush(void)
+{
+}
+
+static char output[1024];
+static size_t output_len;
+
+static void capture_emit_number(int x)
+{
+    output_len += (size_t)snprintf(&output[output_len], sizeof(output) - output_len, "%d", x);
+}
+
+static void capture_emit_wchar(wchar_t wc)
+{
+    output_len += wcrtomb(&output[output_len], wc, NULL);
+    output[output_len] = 0;
+}
+
+static void capture_emit_char(char c)
+{
+    output[output_len++] = c;
+    output[output_len] = 0;
+}
+
+static int testcase(struct config config, const char *input)
+{
+    int r;
+
+    output_len = 0;
+    output[output_len] = 0;
+
+    config.str = input;
+    r = interpret(config);
+
+    buffered_input = NULL;
+
+    return r;
+}
+
+static void test_multibyte(struct config config)
+{
+    char *args[] = { "stdin" };
+    int r;
+
+    config.argc = 1;
+    config.argv = args;
+
+    r = testcase(config, "\xc3");  // First character of multibyte UTF-8 sequence.
+    assert(1 == r);
+
+    buffered_input = "oö";
+    r = testcase(config, "\"aä\" [^$1_=~][,]#%");
+    assert(!strcmp(output, "aäoö"));
+}
+
+static void test_token(struct config config)
+{
+    char *args[] = { "stdin" };
+    int r;
+
+    config.argc = 1;
+    config.argv = args;
+
+    // Stack not empty.
+    r = testcase(config, "42");
+    assert(1 == r);
+
+    r = testcase(config, "42.");
+    assert(0 == r);
+    assert(!strcmp(output, "42"));
+
+    r = testcase(config, "\"hello\"");
+    assert(0 == r);
+    assert(!strcmp(output, "hello"));
+
+    r = testcase(config, "65,");
+    assert(0 == r);
+    assert(!strcmp(output, "A"));
+
+    buffered_input = "hello";
+    r = testcase(config, "^,^,^,");
+    assert(!strcmp(output, "hel"));
+
+    r = testcase(config, "ß");
+    assert(0 == r);
+    assert(!strcmp(output, ""));
+
+    r = testcase(config, "B");
+    assert(0 == r);
+    assert(!strcmp(output, ""));
+
+    r = testcase(config, "{1");
+    assert(1 == r);
+
+    r = testcase(config, "1}");
+    assert(1 == r);
+
+    r = testcase(config, "{1,}");
+    assert(0 == r);
+    assert(!strcmp(output, ""));
+
+    r = testcase(config, "[1");
+    assert(1 == r);
+
+    r = testcase(config, "1]");
+    assert(1 == r);
+
+    r = testcase(config, "{65,[42]}[4{}2]!.',,.");
+    assert(0 == r);
+    assert(!strcmp(output, "2,4"));
+
+    r = testcase(config, "{65,[42]}[4{}2\"!\"[6]]!!.\",\".',,.");
+    assert(0 == r);
+    assert(!strcmp(output, "!6,2,4"));
+
+    r = testcase(config, "1a:a;.");
+    assert(0 == r);
+    assert(!strcmp(output, "1"));
+
+    r = testcase(config, "'{,");
+    assert(0 == r);
+    assert(!strcmp(output, "{"));
+
+    r = testcase(config, "'[,");
+    assert(0 == r);
+    assert(!strcmp(output, "["));
+
+    r = testcase(config, "[1]a:a;!.");
+    assert(0 == r);
+    assert(!strcmp(output, "1"));
+
+    r = testcase(config, "1_.");
+    assert(0 == r);
+    assert(!strcmp(output, "-1"));
+
+    r = testcase(config, "3 a + .");
+    assert(1 == r);
+
+    r = testcase(config, "3 4 + .");
+    assert(0 == r);
+    assert(!strcmp(output, "7"));
+
+    r = testcase(config, "3 4 - .");
+    assert(0 == r);
+    assert(!strcmp(output, "-1"));
+
+    r = testcase(config, "3 4 * .");
+    assert(0 == r);
+    assert(!strcmp(output, "12"));
+
+    r = testcase(config, "20 4 / .");
+    assert(0 == r);
+    assert(!strcmp(output, "5"));
+
+    // Divide-by-zero.
+    r = testcase(config, "1 0 /");
+    assert(1 == r);
+
+    r = testcase(config, "20 4 = .");
+    assert(0 == r);
+    assert(!strcmp(output, "0"));
+
+    // Stack type mismatch.
+    r = testcase(config, "a 0 = .");
+    assert(1 == r);
+
+    r = testcase(config, "1$a:b: a b = .");
+    assert(0 == r);
+    assert(!strcmp(output, "0"));
+
+    r = testcase(config, "ab: a b; = .");
+    assert(0 == r);
+    assert(!strcmp(output, "-1"));
+
+    // Lambda comparison is always false.
+    r = testcase(config, "[3][3]=.");
+    assert(0 == r);
+    assert(!strcmp(output, "0"));
+
+    r = testcase(config, "20 4 > .");
+    assert(0 == r);
+    assert(!strcmp(output, "-1"));
+
+    r = testcase(config, "7 2 & .");
+    assert(0 == r);
+    assert(!strcmp(output, "2"));
+
+    r = testcase(config, "4 2 | .");
+    assert(0 == r);
+    assert(!strcmp(output, "6"));
+
+    r = testcase(config, "1_ ~ .");
+    assert(0 == r);
+    assert(!strcmp(output, "0"));
+
+    r = testcase(config, "3 $ .',,.");
+    assert(0 == r);
+    assert(!strcmp(output, "3,3"));
+
+    r = testcase(config, "3 4 % .");
+    assert(0 == r);
+    assert(!strcmp(output, "3"));
+
+    r = testcase(config, "3 4 \\ .',,.");
+    assert(0 == r);
+    assert(!strcmp(output, "3,4"));
+
+    r = testcase(config, "1 2 3 @ ...");
+    assert(0 == r);
+    assert(!strcmp(output, "132"));
+
+    // ( xu ... x1 x0 u -- xu ... x1 x0 xu )
+    r = testcase(config, "1 2 3 0O ....");
+    assert(0 == r);
+    assert(!strcmp(output, "3321"));
+
+    r = testcase(config, "1 2 3 1O ....");
+    assert(0 == r);
+    assert(!strcmp(output, "2321"));
+
+    r = testcase(config, "1 2 3 2ø ....");
+    assert(0 == r);
+    assert(!strcmp(output, "1321"));
+
+    r = testcase(config, "1 2 3 3O ....");
+    assert(1 == r);
+
+    r = testcase(config, "['T,]t: 0~ t;?");
+    assert(0 == r);
+    assert(!strcmp(output, "T"));
+
+    r = testcase(config, "['F,]f: ['T,]t: 0$f;?~t;?");
+    assert(0 == r);
+    assert(!strcmp(output, "T"));
+
+    r = testcase(config, "[$100\\>]c: [1+]l: 1 c;l;#.");
+    assert(0 == r);
+    assert(!strcmp(output, "100"));
+
+    // Unsupported code injection.
+    r = testcase(config, "`");
+    assert(1 == r);
+
+    // Unknown symbol.
+    r = testcase(config, "A");
+    assert(1 == r);
+}
+
+static void test_arguments(struct config config)
+{
+    char *args[] = { "stdin", "42", "string" };
+    int r;
+
+    config.argc = 0;
+    config.argv = NULL;
+    r = testcase(config, "");
+    assert(1 == r);
+
+    config.argc = 27;
+    config.argv = args;
+    r = testcase(config, "");
+    assert(1 == r);
+
+    config.argc = 1;
+    config.argv = args;
+    r = testcase(config, "");
+    assert(0 == r);
+
+    config.argc = 2;
+    config.argv = args;
+    r = testcase(config, "a;.':,b;.':,c;.");
+    assert(0 == r);
+    assert(!strcmp(output, "1:42:0"));
+
+    config.argc = 3;
+    config.argv = args;
+    r = testcase(config, "");
+    assert(1 == r);
+}
+
+int main(void)
+{
+    struct config config;
+
+    config.fatal       = nop_fatal;
+    config.log_trace   = nop_log_trace;
+    config.log_stack   = NULL;
+    config.emit_number = capture_emit_number;
+    config.emit_wchar  = capture_emit_wchar;
+    config.emit_char   = capture_emit_char;
+    config.input       = input;
+    config.flush       = nop_flush;
+
+    // Test data includes UTF-8 encoded multibyte characters.
+    setlocale(LC_ALL, "en_US.UTF-8");
+
+    {
+        struct token token;
+        storage_set(',', token_make_number(42));
+        token = storage_get(',');
+        assert(token.tok == tokNumber);
+        assert(token.u.number == 0);
+    }
+
+    test_multibyte(config);
+
+    config.log_stack   = nop_log_stack;
+
+    test_token(config);
+
+    test_arguments(config);
+
+    return 0;
+}
