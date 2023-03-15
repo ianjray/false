cea4857612a0504c3be3be719d50702f2b9a4e5e
Ian Ray <ianjray@me.com>
2023-03-15 23:52:47 +0200

    More extensions
    Language extensions that allow (much) richer programs to be developed.
    
    reverse
     * ®
     * reverse the contents of the stack
    
    roll
     * ™
     * move an arbitrary stack item to the top
    
    Signed-off-by: Ian Ray <ianjray@me.com>

--- src/code-point.h
+++ src/code-point.h
@@ -4,6 +4,7 @@ enum {
     POUND_SIGN                                 = L'\u00a3', // £
     SECTION_SIGN                               = L'\u00a7', // §
     LEFT_POINTING_DOUBLE_ANGLE_QUOTATION_MARK  = L'\u00ab', // «
+    REGISTERED_SIGN                            = L'\u00ae', // ®
     RIGHT_POINTING_DOUBLE_ANGLE_QUOTATION_MARK = L'\u00bb', // »
     INVERTED_QUESTION_MARK                     = L'\u00bf', // ¿
     LATIN_CAPITAL_LETTER_O_WITH_STROKE         = L'\u00d8', // Ø
@@ -12,6 +13,7 @@ enum {
     LATIN_SMALL_LETTER_O_WITH_STROKE           = L'\u00f8', // ø
     PER_MILLE_SIGN                             = L'\u2030', // ‰
     EURO_SIGN                                  = L'\u20ac', // €
+    TRADE_MARK_SIGN                            = L'\u2122', // ™
     INTEGRAL                                   = L'\u222b', // ∫
     NOT_EQUAL_TO                               = L'\u2260', // ≠
     LESS_THAN_OR_EQUAL_TO                      = L'\u2264', // ≤
